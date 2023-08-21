c9363b12c78403e2babd2215e462fa14d7ccd382
Ben-hurQB <ben-hurqbeltrao@fac.pe.senac.br>
2023-08-21 20:13:19 +0000

    Fazendo correçao variavel divisor

--- main.py
+++ main.py
@@ -3,19 +3,23 @@
 n = int(input('Informe o número: '))
 lista = []
 primo = [2]
-for elemento in range(1,n+1):
-  lista.append(elemento)
-
-#testar cada elemento da lista e verificar se é primo
-for elemento in range(2,len(lista)):
-  n = lista[elemento]
-  divisor = 3
-  achei = False
-  while divisor <= n:
-    if n % divisor == 0 :
-      achei = True
-      break
-    divisor += 1
-  if  achei  and n // divisor == 1:
-     primo.append(n)
-print(primo)   
+if n > 0:
+  for elemento in range(1,n+1):
+    lista.append(elemento)
+  
+  #testar cada elemento da lista e verificar se é primo
+  
+  for elemento in range(2,len(lista)):
+    n = lista[elemento]
+    divisor = 2
+    achei = False
+    while divisor <= n:
+      if n % divisor == 0 and divisor < n:
+        achei = True
+        break
+      divisor += 1
+    if  not achei:
+       primo.append(n)
+  print(primo)  
+else:
+  print('Não existe número primo para números negativos ou igual a zero')
